a405647748e572843380810b8c882e3228aa1fa3
m-oka-system <m-oka-system@outlook.com>
2023-03-23 10:29:02 +0900

    [update]Speech to Text Appを追加

--- main.py
+++ main.py
@@ -2,10 +2,11 @@ import os
 import streamlit as st
 import azure.cognitiveservices.speech as speechsdk
 from io import BytesIO
+from pytube import YouTube
+from moviepy.editor import *
 
 subscription_key = os.environ.get("SPEECH_KEY")
 region = os.environ.get("SPEECH_REGION")
-voice_name_jp = ["Ja-Jp-Nanamineural", "ja-JP-KeitaNeural"]
 
 
 def text_to_speech(text, voice_option):
@@ -23,6 +24,18 @@ def text_to_speech(text, voice_option):
     )
 
     return speech_synthesizer.speak_text_async(text).get()
+
+
+def speech_to_text(voice_language):
+    speech_config = speechsdk.SpeechConfig(subscription=subscription_key, region=region)
+    speech_config.speech_recognition_language = voice_language
+    audio_config = speechsdk.audio.AudioConfig(filename="audio.wav")
+
+    recognizer = speechsdk.SpeechRecognizer(
+        speech_config=speech_config, audio_config=audio_config
+    )
+    result = recognizer.recognize_once()
+    return result.text
 
 
 def display_speech_synthesis_result(result):
@@ -44,19 +57,47 @@ def display_speech_synthesis_result(result):
                 st.error("APIのキーとリージョンが正しいか確認してください")
 
 
-st.title("Azure Text to Speech App")
-voice_option = st.radio("音声を選択", voice_name_jp)
-input_option = st.radio("入力方法", ["直接入力する", "テキストファイルを読み込む"])
+def download_audio_from_youtube(url):
+    yt = YouTube(url)
+    audio_stream = yt.streams.filter(only_audio=True).first()
+    audio_file = audio_stream.download(filename="audio.mp4")
+    audioclip = AudioFileClip(audio_file)
+    audioclip.write_audiofile("audio.wav", codec="pcm_s16le")
+    os.remove("audio.mp4")
 
-text = ""
-if input_option == "直接入力する":
-    text = st.text_area("こちらにテキストを入力してください：", "Azure Text-to-Speech用のサンプル文です")
-else:
-    uploaded_file = st.file_uploader("テキストファイルをアップロードしてください：", type=["txt"])
-    if uploaded_file:
-        text = uploaded_file.read().decode("utf-8")
 
-if st.button("音声を生成"):
-    if text:
-        result = text_to_speech(text, voice_option)
-        display_speech_synthesis_result(result)
+st.title("Azure Cognitive Services App")
+tab1, tab2 = st.tabs(["Text to Speech", "Speech to Text"])
+
+with tab1:
+    voice_name_jp = ["Ja-Jp-Nanamineural", "ja-JP-KeitaNeural"]
+    voice_option = st.radio("音声を選択", voice_name_jp)
+    input_option = st.radio("入力方法", ["直接入力する", "テキストファイルを読み込む"])
+
+    text = ""
+    if input_option == "直接入力する":
+        text = st.text_area("こちらにテキストを入力してください：", "Azure Text-to-Speech用のサンプル文です")
+    else:
+        uploaded_file = st.file_uploader("テキストファイルをアップロードしてください：", type=["txt"])
+        if uploaded_file:
+            text = uploaded_file.read().decode("utf-8")
+
+    if st.button("音声を生成"):
+        with st.spinner("しばらくお待ちください..."):
+            if text:
+                result = text_to_speech(text, voice_option)
+                display_speech_synthesis_result(result)
+            else:
+                st.error("テキストを入力してください")
+
+with tab2:
+    video_url = st.text_input("YoutubeのURLを入力してEnterを押してください:")
+    if video_url:
+        st.video(video_url)
+
+        voice_language = st.radio("言語を選択", ["ja-JP", "en-US"])
+        if st.button("テキストを生成"):
+            with st.spinner("しばらくお待ちください..."):
+                download_audio_from_youtube(video_url)
+                text = speech_to_text(voice_language)
+                st.info(text)
